7bc36dc976db89d80553e38bb8df76febb1bdd8e
NicolasNara2 <nicolasnaraarg@gmail.com>
2023-08-30 14:56:09 -0300

    Primera subida de ejericicios

---
+++ src/Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+
+    private String nombre;
+    private
+
+}
+
+
+
+
+//Crea un programa que reciba un usuario  y lo guarde en una lista de usuarios.
+//Cada usuario tiene nombre, clave y dirección de correo.
+//Al final debe imprimir todos los usuarios guardados.
+//¿Qué es más conveniente un array o un ArrayList?
